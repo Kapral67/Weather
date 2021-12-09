c949a7534fd56519eb48a41930e3630f7463111d
Maxwell Kapral <mkapral@mail.csuchico.edu>
2021-12-09 15:26:06 +0000

    working on mobile

--- blazor-wasm/weatherFrontEnd/Program.cs
+++ blazor-wasm/weatherFrontEnd/Program.cs
@@ -17,7 +17,7 @@ namespace weatherFrontEnd
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://cdn.maxkapral.com/") });
 
             await builder.Build().RunAsync();
         }
