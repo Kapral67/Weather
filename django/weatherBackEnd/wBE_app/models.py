ef73bfaf8a79ceb259a48ede7f4bb1482a026c70
Maxwell Kapral <mkapral@mail.csuchico.edu>
2022-02-11 00:51:35 +0000

    ugettext_lazy -> gettext_lazy (Django)

--- django/weatherBackEnd/wBE_app/models.py
+++ django/weatherBackEnd/wBE_app/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 # Create your models here.
